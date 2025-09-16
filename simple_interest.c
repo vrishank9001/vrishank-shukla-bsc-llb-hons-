c3663615270b10a9f2a2b458430025d94b00aa06
vrishank9001 <vrishankshukla028@gmail.com>
2025-09-16 21:57:08 +0530

    Create simple_interest.c

---
+++ simple_interest.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    float principal, rate, time, simple_interest;
+
+    printf("Enter principal amount: ");
+    scanf("%f", &principal);
+
+    printf("Enter rate of interest: ");
+    scanf("%f", &rate);
+
+    printf("Enter time (in years): ");
+    scanf("%f", &time);
+
+    simple_interest = (principal * rate * time) / 100;
+    printf("Simple Interest = %.2f\n", simple_interest);
+
+    return 0;
+}
